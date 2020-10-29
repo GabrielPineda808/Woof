ba9cc1aac08b742a833e3240ad9932d9e03ce9a5
Kevin Luo <kvn.luo@gmail.com>
2020-10-28 20:35:48 -0400

    add passport strategory

---
+++ config/passport.js
@@ -0,0 +1,34 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const db = require('../models');
+
+passport.use(new LocalStrategy(
+  { 
+    usernameField: "email" 
+  }, 
+  async function strategy1(email, password, done) {
+    let user = await db.User.findOne({ where: { email: email }});
+    if (!user) {
+      return done(null, false, { message: "Incorrect email."})
+    };
+  
+    let pwValidate = await user.validPassword(password);
+    if (!pwValidate) {
+      return done(null, false, { message: "Incorrect password."})
+    };
+  
+    return done(null, user);
+  })
+)
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+})
+
+passport.deserializeUser(async function(id, done) {
+  let user = await db.User.findOne({ where: { id: id}});
+  done(null, user);
+})
+
+module.exports = passport;
