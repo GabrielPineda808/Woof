a15b967b13736c5a06780a7fc90c1bfff965c617
Brian Lee <brianlee@Brians-MBP.hsd1.ca.comcast.net>
2020-10-30 14:56:26 -0700

    model connections

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 var bcrypt = require("bcryptjs")
 
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define("User", {
     // The email cannot be null, and must be a proper email before creation
     email: {
@@ -17,23 +17,41 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false
     },
     name: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     gender: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     bio: {
-        type: DataTypes.STRING,
-        allowNull: true
+      type: DataTypes.STRING,
+      allowNull: true
     }
   });
-  User.prototype.validPassword = function(password) {
+  User.prototype.validPassword = function (password) {
     return bcrypt.compare(password, this.password);
   };
-  User.addHook("beforeCreate", function(user) {
+  User.addHook("beforeCreate", function (user) {
     user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10));
   });
+  // User has many dogs
+  User.associate = function (models) {
+    User.hasMany(models.Dog, {
+      onDelete: "cascade"
+    });
+  };
+  // User has many reviews
+  User.associate = function (models) {
+    User.hasMany(models.UserReview, {
+      onDelete: "cascade"
+    });
+  };
+// For the User's reviews on other dogs
+  User.associate = function(models) {
+    User.hasMany(models.dogReview, {
+      onDelete: "cascade"
+    });
+  };
   return User;
 };
