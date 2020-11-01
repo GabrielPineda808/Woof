04fe5ad91babcdcadd0e5ee5b19db77cb6c5c1da
Gabriel Pineda <gabepineda6@gmail.com>
2020-11-01 14:42:13 -0800

    changed the review.js in order to attempt to add more reviews and read the file, in userprofile front end made reviews into a list

--- public/assets/js/reviews.js
+++ public/assets/js/reviews.js
@@ -1,99 +1,133 @@
-var $noteTitle = $("#username");
-var $noteText = $("#review");
-var $newNoteBtn = $("#submitBtn");
+var $Username = $("#username");
+var $review = $("#review");
+var $newReviewBtn = $("#submitBtn");
+var $reviewList = $(" .list-group");
 
-// activeNote is used to keep track of the note in the textarea
-var activeNote = {};
+// // activereview is used to keep track of the note in the textarea
+// var activereview = {};
 
-// A function for getting all notes from the db
-var getNotes = function() {
-  return $.ajax({
-    url: "/api/notes",
-    method: "GET"
-  });
-};
+// // A function for getting all notes from the db
+// var getReview = function() {
+//   return $.ajax({
+//     url: "/api/notes",
+//     method: "GET"
+//   });
+// };
 
-// A function for saving a note to the db
-var saveNote = function(note) {
-  return $.ajax({
-    url: "/api/notes",
-    data: note,
-    method: "POST"
-  });
-};
-
-
-// Get the note data from the inputs, save it to the db and update the view
-var handleNoteSave = function() {
-  var newNote = {
-    title: $noteTitle.val(),
-    text: $noteText.val()
-  };
-
-  saveNote(newNote).then(function(data) {
-    getAndRenderNotes();
-    renderActiveNote();
-  });
-};
-
-// Sets the activeNote to and empty object and allows the user to enter a new note
-var handleNewNoteView = function() {
-  activeNote = {};
-  renderActiveNote();
-};
+// // A function for saving a note to the db
+// var saveReview = function(note) {
+//   return $.ajax({
+//     url: "/api/notes",
+//     data: note,
+//     method: "POST"
+//   });
+// };
 
 
-// If a note's title or text are empty, hide the save button
-// Or else show it
-var handleRenderSaveBtn = function() {
-  if (!$noteTitle.val().trim() || !$noteText.val().trim()) {
-    $newNoteBtn.hide();
-  } else {
-    $newNoteBtn.show();
-  }
-};
+// // Get the note data from the inputs, save it to the db and update the view
+// var handleReviewSave = function() {
+//   var newNote = {
+//     title: $Username.val(),
+//     text: $review.val()
+//   };
 
-// Render's the list of note titles
-var renderNoteList = function(notes) {
-  $noteList.empty();
+//   saveReview(newNote).then(function(data) {
+//     getAndRenderReview();
+//     renderActiveReview();
+//   });
+// };
 
-  var noteListItems = [];
+// // Sets the activereview to and empty object and allows the user to enter a new note
+// var handleNewReviewView = function() {
+//   activereview = {};
+//   renderActiveReview();
+// };
 
-  for (var i = 0; i < notes.length; i++) {
-    var note = notes[i];
 
-    var div = $("<div class='row'>");
-    var newDiv = $("<div class='col-md-2'>");
-    var img = $("<img src='https://image.ibb.co/jw55Ex/def_face.jpg' class='img img-rounded img-fluid'/>")
-    var divv = $("<div class='col-md-10'>")
-    var ptag = $("<p>")
-    var atag = $('<a class="float-left" href="#"><strong>Other User</strong></a>')
-    var ddiv = $('<div class="clearfix">')
+// // If a note's title or text are empty, hide the save button
+// // Or else show it
+// var handleRenderSaveBtn = function() {
+//   if (!$Username.val().trim() || !$review.val().trim()) {
+//     $newReviewBtn.hide();
+//   } else {
+//     $newReviewBtn.show();
+//   }
+// };
+
+// // Render's the list of note titles
+// var renderReviewList = function(reviews) {
+//   $reviewList.empty();
+
+//   var reviewListItems = [];
+
+//   for (var i = 0; i < reviews.length; i++) {
+//     var review = reviews[i];
+
+//     var li = $("<li>")
+//     var div = $("<div class='row'>");
+//     var newDiv = $("<div class='col-md-2'>");
+//     var img = $("<img src='https://image.ibb.co/jw55Ex/def_face.jpg' class='img img-rounded img-fluid'/>")
+//     var divv = $("<div class='col-md-10'>")
+//     var ptag = $("<p>")
+//     var atag = $('<a class="float-left" href="#"><strong>Other User</strong></a>')
+//     var tagp = $("<p>").data(review)
+//     var ddiv = $('<div class="clearfix">')
     
-    div.append(newDiv)
-    newDiv.append(img)
-    div.append(divv)
-    divv.append(ptag)
-    ptag.append(atag)
-    divv.append(ddiv)
-    ddiv.append(ptag)
+//     li.append(div)
+//     div.append(newDiv)
+//     newDiv.append(img)
+//     div.append(divv)
+//     divv.append(ptag)
+//     ptag.append(atag)
+//     divv.append(ddiv)
+//     ddiv.append(tagp)
 
-    noteListItems.push(div);
-  }
+//     reviewListItems.push(li);
+//   }
 
-  $noteList.append(noteListItems);
-};
+//   $reviewList.prepend(reviewListItems);
+// };
 
-// Gets notes from the db and renders them to the sidebar
-var getAndRenderNotes = function() {
-  return getNotes().then(function(data) {
-    renderNoteList(data);
-  });
-};
+// // Gets notes from the db and renders them to the sidebar
+// var getAndRenderReview = function() {
+//   return getReview().then(function(data) {
+//     renderReviewList(data);
+//   });
+// };
 
-$newNoteBtn.on("click", handleNewNoteView);
-$noteTitle.on("keyup", handleRenderSaveBtn);
-$noteText.on("keyup", handleRenderSaveBtn);
+// $newReviewBtn.on("click", handleNewReviewView);
+// $Username.on("keyup", handleRenderSaveBtn);
+// $review.on("keyup", handleRenderSaveBtn);
 
-// Gets and renders the initial list of notes
-getAndRenderNotes();
+// // Gets and renders the initial list of notes
+// getAndRenderReview();
+
+$newReviewBtn.on("click", function(e){
+  e.preventDefault()
+  var review = $review.val()
+  var username = $Username.val()
+  var reviewListItems = [];
+
+  var li = $("<li>")
+  var div = $("<div class='row'>");
+  var newDiv = $("<div class='col-md-2'>");
+  var img = $("<img src='https://image.ibb.co/jw55Ex/def_face.jpg' class='img img-rounded img-fluid'/>")
+  var divv = $("<div class='col-md-10'>")
+  var ptag = $("<p>")
+  var atag = $('<a class="float-left" href="#"><strong>' + username +'</strong></a>')
+  var tagp = $("<p>").data(review)
+  var ddiv = $('<div class="clearfix">')
+    
+  li.append(div)
+  div.append(newDiv)
+  newDiv.append(img)
+  div.append(divv)
+  divv.append(ptag)
+  ptag.append(atag)
+  divv.append(ddiv)
+  ddiv.append(tagp)
+
+  reviewListItems.push(li);
+
+  $reviewList.prepend(reviewListItems);
+})
