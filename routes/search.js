15bfda68473b0c0a37f84e91eed07a623481929e
Kevin Luo <kvn.luo@gmail.com>
2020-11-02 13:56:41 -0500

    dynamic dropdown search page

--- routes/search.js
+++ routes/search.js
@@ -4,10 +4,13 @@ const { forwardAuthenticated, ensureAuthenticated } = require('../config/middlew
 const db = require('../models');
 // routes for searches
 
-router.get('/', ensureAuthenticated ,(req, res) => {
+router.get('/', ensureAuthenticated, async (req, res) => {
   // TODO: replace this view with search page
-
-  res.render('search');
+  let breeds = await db.dog.aggregate('breed', 'DISTINCT', { plain: false });
+  let ages =  await db.dog.aggregate('age', 'DISTINCT', { plain: false });
+  let temperaments =  await db.dog.aggregate('temperament', 'DISTINCT', { plain: false });
+  // console.log(breeds, ages, temperaments);
+  res.render('search', { breeds, ages, temperaments });
 })
 
 router.post('/', async (req, res) => {
