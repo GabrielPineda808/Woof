693ffbb90f31136d488818cc9984c274c278114d
Kevin Luo <kvn.luo@gmail.com>
2020-11-03 08:56:43 -0500

    add dog review section to profile

--- public/assets/js/reviews.js
+++ public/assets/js/reviews.js
@@ -2,12 +2,16 @@ const $username = $("#username").val();
 const $reviewBody = $("textarea#reviewBody").val();
 const $userReviewBtn = $("#userReviewBtn");
 const $rating = $("input[name='stars']").val();
+const $dogSubmitBtn = $("#dogSubmitBtn");
 
+$dogSubmitBtn.on("click", function() {
 
-$userReviewBtn.on("click", function(){
+});
+
+$userReviewBtn.on("click", function() {
   let path = window.location.pathname.split('/');
   let posteeId = path[path.length-1];
   let newReview = { body: $reviewBody, rating: $rating, posterEmail: $username, userId: posteeId}
   console.log(newReview);
   
-})
+});
